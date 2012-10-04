e78747b85b8a5cb55d31691b980824993286b188
Kevin Chabowski <kevin@kch42.de>
2012-10-04 16:19:29 +0200

    New docu and fixed mkdocu.sh
    mkdocu.sh will now test, if the executable is named "naturaldocs" or
    "NaturalDocs".

--- mkdocu.sh
+++ mkdocu.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+if ! ndpath=$(which NaturalDocs 2>/dev/null); then
+if ! ndpath=$(which naturaldocs 2>/dev/null); then
+echo "NaturalDocs could not be found!" >/dev/stderr
+exit 1
+fi; fi
+
+
 if [ ! -d docu/nd ]; then mkdir docu/nd; fi
 if [ ! -d docu/nd_project_dir ]; then mkdir docu/nd_project_dir; fi
-NaturalDocs -i . -o html docu/nd -p docu/nd_project_dir
+$ndpath -i . -o html docu/nd -p docu/nd_project_dir
