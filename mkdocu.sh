f18ecd10c746641a9f46392f24d8d87ed60d2de6
Kevin Chabowski <kevin@kch42.de>
2013-10-25 22:26:24 +0200

    Removed source code documentation. Can be generated at any point.

--- mkdocu.sh
+++ mkdocu.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-if ! ndpath=$(which NaturalDocs 2>/dev/null); then
-if ! ndpath=$(which naturaldocs 2>/dev/null); then
+if ! ndpath=`which NaturalDocs 2>/dev/null`; then
+if ! ndpath=`which naturaldocs 2>/dev/null`; then
 echo "NaturalDocs could not be found!" >/dev/stderr
 exit 1
 fi; fi
 
 
-if [ ! -d docu/nd ]; then mkdir docu/nd; fi
-if [ ! -d docu/nd_project_dir ]; then mkdir docu/nd_project_dir; fi
+test -d docu/nd || mkdir docu/nd
+test -d docu/nd_project_dir || mkdir docu/nd_project_dir
 $ndpath -i . -o html docu/nd -p docu/nd_project_dir
