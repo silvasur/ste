45d8f703d7cbcdf21248e5ad7aafb6b68054d258
Kevin Chabowski <kevin@kch42.de>
2014-05-25 12:12:16 +0200

    Fixed backward compatibility with pre 1.0 versions.

--- src/ste/VariableNode.php
+++ src/ste/VariableNode.php
@@ -6,7 +6,7 @@ class VariableNode extends ASTNode {
 	public $name;
 	public $arrayfields = array();
 	public function transcompile() {
-		$varaccess = '@$ste->vars[' . (is_numeric($this->name) ? $this->name : '"' . Misc::escape_text($this->name) . '"'). ']';
+		$varaccess = '@$ste->scope[' . (is_numeric($this->name) ? $this->name : '"' . Misc::escape_text($this->name) . '"'). ']';
 		foreach($this->arrayfields as $af) {
 			if((count($af) == 1) and ($af[0] instanceof TextNode) and is_numeric($af[0]->text)) {
 				$varaccess .= '[' . $af->text . ']';
